22414154368e7350db12212a51a08ee209a5b88c
rezadlv <reza.dalvand78@gmail.com>
2021-10-20 21:15:08 +0330

    oky

--- src/components/common/Headers/Header.jsx
+++ src/components/common/Headers/Header.jsx
@@ -3,23 +3,27 @@ import React from "react";
 const Header=()=>{
         return(
                 <React.Fragment>
-                        <header className="background">
-                                <div>
-                                        <ul>
-                                                <li>study</li>
-                                                <li>musicvideo</li>
-                                                <li>musicfilm</li>
-                                                <li>iranPop</li>
-                                                <li>pop</li>
-                                                <li>traditinal</li>
-                                        </ul>
-                                </div>
-                                <div>
-                                        <input type="search" />
-                                        <a href=""><i class="fas fa-home"></i></a>
-                                        <a href=""><i class="fas fa-user"></i></a>
-                                        <a href=""><i class="fas fa-gamepad"></i></a>
-                                        <a href=""><i class="fab fa-spotify"></i></a>
+                        <header>
+                                <div className="headers">
+                                        <div className="headerStartRigth">
+                                                <ul>
+                                                        <li>study</li>
+                                                        <li>musicvideo</li>
+                                                        <li>musicfilm</li>
+                                                        <li>iranPop</li>
+                                                        <li>pop</li>
+                                                        <li>traditinal</li>
+                                                </ul>
+                                        </div>
+                                        <div className="headerStartCenter">
+                                                <input type="search" placeholder="search" />
+                                        </div>
+                                        <div className="headerStartLeft">
+                                                <a href=""><i class="fas fa-home"></i></a>
+                                                <a href=""><i class="fas fa-user"></i></a>
+                                                <a href=""><i class="fas fa-gamepad"></i></a>
+                                                <a href=""><i class="fab fa-spotify"></i></a>
+                                        </div>
                                 </div>
                         </header>
                 </React.Fragment>
